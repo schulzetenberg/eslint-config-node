2d2b6eb5ada151478ff2d1466a406949a7e7dee2
Jacob <jacob.schulzetenberg@gmail.com>
2022-01-09 19:20:00 -0600

    Update ECMA version, fix end of line issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,36 @@
 module.exports = {
-  "extends": [
-    "airbnb-base", // Airbnb's base JS .eslintrc (without React plugins),
-    "prettier" // Ignore rules managed by prettier
+  extends: [
+    'airbnb-base', // Airbnb's base JS .eslintrc (without React plugins),
+    'prettier', // Ignore rules managed by prettier
   ],
 
-  // Override the airbnb rules here
-  "rules": {
-    "prefer-promise-reject-errors": 0,
-    "no-use-before-define": 0,
-    "consistent-return": 0,
-    "max-len": ["error", { "code": 120 }],
-
-    // prettier plugin
-    "prettier/prettier": [
-      "error",
-      {
-        "printWidth": 120,
-        "singleQuote": true,
-        "tabWidth": 2,
-        "useTabs": false,
-        "semi": true,
-        "arrowParens": "always",
-        "trailingComma": "es5",
-        "bracketSpacing": true
-      }
-    ]
+  parserOptions: {
+    ecmaVersion: 2020,
   },
 
-  "plugins": ["prettier"]
+  // Override the airbnb rules here
+  rules: {
+    'prefer-promise-reject-errors': 0,
+    'no-use-before-define': 0,
+    'consistent-return': 0,
+    'max-len': ['error', { code: 120 }],
+
+    // prettier plugin
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 120,
+        singleQuote: true,
+        tabWidth: 2,
+        useTabs: false,
+        semi: true,
+        arrowParens: 'always',
+        trailingComma: 'es5',
+        bracketSpacing: true,
+        endOfLine: 'lf',
+      },
+    ],
+  },
+
+  plugins: ['prettier'],
 };
